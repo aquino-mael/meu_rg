b83e1d486ef3b524f1b1276485d8e45156705c05
Ismael Aquino <ismaellAquino@hotmail.com>
2021-10-02 17:02:31 -0300

    move login menu to correct file

---
+++ lib/screens/initial/widgets/login_menu.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+import '../../../app/app.dart';
+
+class LoginMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: IntrinsicWidth(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Spacer(),
+            Text(
+              'ACESSAR CONTA',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headline4.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0)
+                ),
+                labelText: 'E-mail do responsável',
+              ),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0)
+                ),
+                labelText: 'Senha de acesso',
+              ),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            ElevatedButton(
+              child: Text('ENTRAR'),
+              onPressed: () {},
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            ElevatedButton(
+              child: Text('ESQUECI A SENHA'),
+              onPressed: () {},
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            Text(
+              'AINDA NÃO POSSUI UMA CONTA?',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Color(0xFF323234),
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 4.0,
+            ),
+            RichText(
+              textAlign: TextAlign.center,
+              textScaleFactor: 1,
+              text: TextSpan(
+                style: TextStyle(
+                  color: AppColors.primaryColor,
+                  fontSize: 15.0,
+                ),
+                children: [
+                  TextSpan(
+                    style: TextStyle(
+                      decoration: TextDecoration.underline
+                    ),
+                    text: 'CLIQUE AQUI',
+                  ),
+                  TextSpan(
+                    text: ' PARA FAZER UMA',
+                  ),
+                ]
+              ),
+            ),
+            Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/initial/widgets/widgets.dart
+++ lib/screens/initial/widgets/widgets.dart
@@ -1,1 +1,2 @@
+export 'login_menu.dart';
 export 'my_persistent_header.dart';
